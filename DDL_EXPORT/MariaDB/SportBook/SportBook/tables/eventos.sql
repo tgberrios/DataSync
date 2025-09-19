d24256e26ce26a41b09e53ef73744115a8e63a00
Tomy Gustavo Berrios Berrios <tgberrios@outlook.com>
2025-09-19 17:04:28 -0600

    WORKING AS EXPECTED FIXED SOME ISSUES

--- DDL_EXPORT/MariaDB/SportBook/SportBook/tables/eventos.sql
+++ DDL_EXPORT/MariaDB/SportBook/SportBook/tables/eventos.sql
@@ -1,7 +1,7 @@
 -- Table DDL for SportBook.eventos
 -- Engine: MariaDB
 -- Database: SportBook
--- Generated: 1758320182
+-- Generated: 1758322950
 
 CREATE TABLE `eventos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
--- DDL_EXPORT/MongoDB/testdb;/testdb;/tables/ventas.sql
+++ DDL_EXPORT/MongoDB/testdb;/testdb;/tables/ventas.sql
@@ -1,7 +1,7 @@
 -- Table DDL for testdb;.ventas
 -- Engine: MongoDB
 -- Database: testdb;
--- Generated: 1758320182
+-- Generated: 1758322950
 
 -- MongoDB View: ventas
 -- Schema: testdb;
