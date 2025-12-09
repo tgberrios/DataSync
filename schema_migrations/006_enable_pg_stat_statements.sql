5d559a2889fbaf8f71f40cb36aec77959f729112
tgberrios <tbberrios@outlook.com>
2025-12-09 12:51:37 -0600

    Add pg_stat_statements extension check and enable script
    - Create script to enable pg_stat_statements extension
    - Add check in QueryStoreCollector to verify extension exists
    - Log warning if extension is not enabled
    - Extension is required for query snapshot collection

---
+++ schema_migrations/006_enable_pg_stat_statements.sql
@@ -0,0 +1,4 @@
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
+COMMENT ON EXTENSION pg_stat_statements IS 'Enables tracking of execution statistics of all SQL statements executed by a server';
+
